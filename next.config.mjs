7ccede51f8801dfd8c12b28c87fe072361fd5687
AbdulSamad18090 <abdulsamad18090@gmail.com>
2024-11-19 09:19:08 +0500

    allow domain for image in next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'platform-lookaside.fbsbx.com', // Add Facebook platform domain here
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
