6aef5d71c141d0e86868fc0d555104da07b5d298
AbdulSamad18090 <abdulsamad18090@gmail.com>
2025-03-15 19:18:29 +0500

    set api for fetching insppections

--- app/api/inspection/get/route.js
+++ app/api/inspection/get/route.js
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 export async function GET(req) {
   try {
     await dbConnect();
-    const inspections = await Inspection.find();
+    const inspections = await Inspection.find().lean();
     if (inspections.length === 0) {
       return NextResponse.json({
         message: "No Inspections found",
